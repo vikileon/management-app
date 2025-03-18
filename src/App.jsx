09d96084195304c32ea812de0a11f5af63134408
Daria <daria.tim1@yahoo.com>
2025-03-18 21:35:04 +0300

    added collecting and validating  user input, handling project creation, showing an error modal functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,50 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject.jsx";
 import Sidebar from "./components/Sidebar.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const projectId = Math.random()
+      const newProject = {
+        ...projectData,
+        id: projectId,
+      };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NoProjectSelected />
+      <Sidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
+      {content}
     </main>
   );
 }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,26 +1,66 @@
-import Input from "./Input.jsx";
+import { useRef } from "react";
 
-function NewProject() {
+import Input from "./Input.jsx";
+import Modal from "./Modal.jsx";
+
+function NewProject({ onAdd }) {
+  const modal = useRef();
+
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim === "" ||
+      enteredDueDate === ""
+    ) {
+      modal.current.open()
+      return;
+    }
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+
   return (
-    <div className="w-[35rem] mt-16 ">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950 font-medium">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input label="Title" />
-        <Input label="Description" isTextarea />
-        <Input label="Due Date" />
+    <>
+    <Modal ref={modal} buttonCaption='Close'>
+      <h2>Invalid Input</h2>
+      <p>Oops... Looks like you forgot to enter a value.</p>
+      <p>Please make sure you provide valid value for every input field</p>
+    </Modal>
+      <div className="w-[35rem] mt-16 ">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950 font-medium">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" isTextarea />
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
